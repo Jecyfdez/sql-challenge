e1bbc9ef2bc3e7fe8c02402b5e3fa67c94451280
Jessica Fernandez <jessicafernandez@Jessicas-MacBook-Pro.local>
2020-10-19 17:36:32 -0700

    updates

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -53,19 +53,49 @@ SELECT * FROM salaries;
 SELECT * FROM titles;
 
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-
+CREATE VIEW "Employee Details" AS
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+INNER JOIN salaries AS s ON 
+e.emp_no = s.emp_no
+--1. FINDINGS
+SELECT *
+FROM "Employee Details";
 
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
-
-
+CREATE VIEW "Employees Hired in 1986" AS
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+--2. FINDINGS
+SELECT *
+FROM "Employees Hired in 1986"
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
+CREATE VIEW "Department Manager Details" AS
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_managers AS dm
+LEFT JOIN employees AS e 
+ON e.emp_no = dm.emp_no
+LEFT JOIN departments AS d
+ON dm.dept_no = d.dept_no;
+--3. FINDINGS
+SELECT *
+FROM "Department Manager Details";
 
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-
-
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name
+CREATE VIEW "Employee Department Details" AS
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+LEFT JOIN dept_emp AS de 
+ON e.emp_no = de.emp_no
+LEFT JOIN departments AS d
+ON de.dept_no = d.dept_no;
+--4. FINDINGS
+SELECT *
+FROM "Employee Department Details";
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
